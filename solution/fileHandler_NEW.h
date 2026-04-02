146cffeac3b5c9b4f11c6fd7193671048faf8072
Richard <rcihardking@outlook.com>
2026-04-02 08:36:48 +0100

    add3

--- solution/fileHandler_NEW.h
+++ solution/fileHandler_NEW.h
@@ -15,6 +15,8 @@
 #include <glad/glad.h>
 #include <libpng/png.h>
 
+#include "hashtable.h"
+
 
 //defining structs used by functions
 //images
@@ -51,33 +53,24 @@ image readPNG(std::string filepath);
 
 //meshes
 std::vector<float> readOBJ(std::string filepath);
-
+std::vector<float> readOBJ_old(std::string filepath);
 
 
 
 //defining default dictionaries
 typedef std::vector<float>(*readMeshPtr)(std::string);
-std::map<std::string, readMeshPtr> default_meshFormatLoaders = {
-	{"obj", &readOBJ}
-};
-
-typedef image (*readImgPtr)(std::string);
-std::map<std::string, readImgPtr> default_imageFormatLoaders = {
-	{"png", &readPNG}
-};
-
-
+typedef image(*readImgPtr)(std::string);
 
 //defining file loading classes
 struct formatLoaders {
-	std::map<std::string, readImgPtr> imgFormats;
-	std::map<std::string, readMeshPtr> meshFormats;
+	hashtable<readImgPtr, 10>* imgFormats;
+	hashtable<readMeshPtr, 10>* meshFormats;
 };
 
 
 class fileLoader {
 public:
-	fileLoader(formatLoaders formatFunctions);
+	fileLoader(formatLoaders formatFunctions) : formats{ formatFunctions } {};
 
 	texture imageLoader(std::string filepath);
 	mesh meshLoader(std::string filepath);
